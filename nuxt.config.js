0e1d9af79b174fd514f1f7d63db10a941f996529
jlo <ts01545387@gmail.com>
2025-10-24 18:41:26 +0800

    edit baseURL

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  base: "/",
+  baseURL: "/todoList/",
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: [
